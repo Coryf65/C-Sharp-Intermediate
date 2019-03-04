aa15db6cfb0ddff1ea1e4f4d702876d006451b0d
Cory <Coryf65@gmail.com>
2019-03-04 12:24:37 -0600

    Writing your first Linq Query: done

--- Program.cs
+++ Program.cs
@@ -19,6 +19,40 @@ namespace C_Sharp_Intermediate
             //Lambda Expressions
             //Object and Collection Initializers
 
+            #region "Writing Your First LINQ Query"
+
+
+            List<int> numbers = new List<int> { 2, 4, 8, 16, 32, 64 };
+            List<int> numbersGreaterThanTen = new List<int>();
+            List<int> numsGreaterThan10Linq = new List<int>();
+           
+
+            // Looping to find nums > 10
+            foreach (int number in numbers)
+            {
+                if (number > 10)
+                {
+                    numbersGreaterThanTen.Add(number);
+                }
+            }
+
+            //display results
+            Console.WriteLine("using the for each loop...");
+            Console.WriteLine("Nums > 10: " + numbersGreaterThanTen);
+            //result: { 16, 32, 64}
+
+            //numsGreaterThan10Linq = from numbers where numbers > 10 select number;
+
+            // shorthand and displaying
+            var linqResults = from n in numbers where n > 10 select n;
+
+            // Now using LINQ
+            Console.WriteLine("using LINQ...");
+            Console.WriteLine("Nums > 10: " + linqResults);
+
+            // NOTE: LINQ uses deffered execution
+
+            #endregion
 
 
         }
