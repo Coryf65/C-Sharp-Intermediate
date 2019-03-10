83fe9f971fcffa7a7fce598d8563952416898fda
Cory <Coryf65@gmail.com>
2019-03-10 13:34:06 -0500

    Moved sections into seperate classes.

--- Program.cs
+++ Program.cs
@@ -11,66 +11,15 @@ namespace C_Sharp_Intermediate
         static void Main(string[] args)
         {
 
-            //C# features that support LINQ:
+            QueryingWithLinq queryingWithLinq = new QueryingWithLinq();
 
-            //Anonymous Types
-            //Extension Methods
-            //Implicit Typing
-            //Lambda Expressions
-            //Object and Collection Initializers
+            queryingWithLinq.Run();
 
-            #region "Writing Your First LINQ Query"
-
-
-            List<int> numbers = new List<int> { 2, 4, 8, 16, 32, 64 };
-            List<int> numbersGreaterThanTen = new List<int>();
-            List<int> numsGreaterThan10Linq = new List<int>();
-           
-
-            // Looping to find nums > 10
-            foreach (int number in numbers)
-            {
-                if (number > 10)
-                {
-                    numbersGreaterThanTen.Add(number);
-                }
-            }
-
-            //display results
-            Console.WriteLine("using the for each loop...");
-            Console.WriteLine("Nums > 10: " + numbersGreaterThanTen);
-            //result: { 16, 32, 64}
-
-            //numsGreaterThan10Linq = from numbers where numbers > 10 select number;
-
-            // shorthand and displaying
-            var linqResults = from n in numbers where n > 10 select n;
-
-            // Now using LINQ
-            Console.WriteLine("using LINQ...");
-            Console.WriteLine("Nums > 10: " + linqResults);
-
-            // NOTE: LINQ uses deffered execution
-
-            
-
-            #endregion
-
+            // pausing to read input
+            Console.ReadKey();
 
         }
 
-        /// <summary>
-        ///  Challenge 1 returning values
-        /// </summary>
-        /// <param name="list"></param>
-        /// <returns></returns>
-        public IEnumerable<int> NumbersGreaterThanFive(List<int> list)
-        {
-            IEnumerable<int> lessThanFive;
-
-            lessThanFive = from n in list where n > 5 select n; 
-
-            return lessThanFive;
-        }
+        
     }
 }
---
+++ QueryingWithLinq.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Intermediate
+{
+    class QueryingWithLinq
+    {
+
+        //C# features that support LINQ:
+
+        //Anonymous Types
+        //Extension Methods
+        //Implicit Typing
+        //Lambda Expressions
+        //Object and Collection Initializers
+
+        //"Writing Your First LINQ Query"
+
+        public QueryingWithLinq()
+        {
+            
+        }
+
+        public void Run()
+        {
+            List<int> numbers = new List<int> { 2, 4, 8, 16, 32, 64 };
+            List<int> numbersGreaterThanTen = new List<int>();
+            List<int> numsGreaterThan10Linq = new List<int>();
+
+
+            // Looping to find nums > 10
+            foreach (int number in numbers)
+            {
+                if (number > 10)
+                {
+                    numbersGreaterThanTen.Add(number);
+                }
+            }
+
+            //display results
+            Console.WriteLine("using the for each loop...");
+            Console.WriteLine("Nums > 10: " + numbersGreaterThanTen);
+            //result: { 16, 32, 64}
+
+            //numsGreaterThan10Linq = from numbers where numbers > 10 select number;
+
+            // shorthand and displaying
+            var linqResults = from n in numbers where n > 10 select n;
+
+            // Now using LINQ
+            Console.WriteLine("using LINQ...");
+            Console.WriteLine("Nums > 10: " + linqResults);
+
+            // NOTE: LINQ uses deffered execution
+        }
+
+        /// <summary>
+        ///  Challenge 1 returning values
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public IEnumerable<int> NumbersGreaterThanFive(List<int> list)
+        {
+            IEnumerable<int> lessThanFive;
+
+            lessThanFive = from n in list where n > 5 select n;
+
+            return lessThanFive;
+        }
+   } 
+}
